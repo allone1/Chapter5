0f9f835016e94ea162d9b13b2023df68f583faa5
LaTonya Harrison <allone1@users.noreply.github.com>
2016-03-07 19:11:17 -0700

    Add .getJSON code to print out to console the entire object Flickr responds to .getJSON with.

--- Flickr/js/app.js
+++ Flickr/js/app.js
@@ -1,8 +1,17 @@
 var main = function () {
 	"use strict";
 	
-	console.log("Hello World!");
-	
+	// this is actually just one string,
+	// but I spread it out over two lines
+	// to make it easier to read.
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
+		"tags=dogs&format=json&jsoncallback=?"; 
+		
+	$.getJSON(url, function (flickrResponse) {
+		// we'll simply print the response to the console
+		// for the time being
+		console.log(flickrResponse);		
+	});
 };
 
 $(document).ready(main);
